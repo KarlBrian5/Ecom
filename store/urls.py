509fd898a805da97187d7958ff7f36ae0b8eba78
KarlBria5 <bryanblekinson27@gmail.com>
2020-06-03 16:37:38 +0300

    Login Page added

--- store/urls.py
+++ store/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	#Leave as empty string for base url
-	path('', views.store, name="store"),
-	path('cart/', views.cart, name="cart"),
-	path('checkout/', views.checkout, name="checkout"),
+    # Leave as empty string for base url
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('login/', views.loginPage, name="login"),
+    path('register/', views.registerPage, name="register"),
 
-	path('update_item/', views.updateItem, name="update_item"),
-	path('process_order/', views.processOrder, name="process_order"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import json
 import datetime
 from .models import *
 from .utils import cookieCart, cartData, guestOrder
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'store/register.html', context)
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password )
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username Or password is Incorrect')
+
+
+    context = {}
+    return render(request, 'store/login.html', context)
 
 
 def store(request):
